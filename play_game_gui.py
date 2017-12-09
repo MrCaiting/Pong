b43c245f3652415d16859c4c80e2e6db1f7c96fa
MrCaiting <mrctwu@gmail.com>
2017-12-09 04:09:27 -0600

    GUI need testing

--- play_game_gui.py
+++ play_game_gui.py
@@ -1,6 +1,10 @@
 import pygame as pyg
 import sys
-#import pong_continuous
+import pong_continuous as pc
+# Parameters used for training
+TRAIN_TRAIL = 1000000
+Q_Dict = dict()
+Action_Dict = dict()
 
 # Frame per second that we set up and use in the GUI
 FPS = 30
@@ -19,6 +23,9 @@ WHOLE_WINDOW = ((ZERO, ZERO), (VIEW_WIDTH, VIEW_HEIGHT))
 
 # Detail of the paddle
 PAD_THICK = 5
+L_PAD_LEN = VIEW_HEIGHT * pc.LP_HEIGHT
+R_PAD_LEN = VIEW_HEIGHT * pc.RP_HEIGHT
+
 # Detail of the ping-pong table
 COURT_LINE = 2
 RADIUS = 5
@@ -26,6 +33,7 @@ RADIUS = 5
 pyg.init()
 SCREEN = pyg.display.set_mode((VIEW_WIDTH, VIEW_HEIGHT))
 pyg.display.set_caption("Single Player Pong")
+
 
 def showPaddle(paddle_area, position):
     """ Function used to draw the paddle """
@@ -60,19 +68,81 @@ def getUpdateValue(state):
     return new_ball_x, new_ball_y, new_right_pad, new_left_pad
 
 
-def displayState(state, ball, l_pad, r_pad, round):
+def displayState(state, l_pad, r_pad, round_count):
     ball_x, ball_y, right_pad, left_pad = getUpdateValue(state)
     # Start re-drawing the court
     showTable()
     showPaddle(l_pad, (l_pad.x, left_pad))
     showPaddle(r_pad, (r_pad.x, right_pad))
     showBall((ball_x, ball_y))
+    # Display game status
+    text = pyg.font.Font('None', 15)
+    surface = text.render("Round #: {}", format(round_count), BLACK)
+    textbox = surface.get_rect()
+    textbox.topleft = (VIEW_WIDTH / 2, 28)
+    SCREEN.blit(surface, textbox)
 
 
-clock = pyg.time.clock()
+clock = pyg.time.Clock()
+# Right paddle sizing
+right_pad_x = VIEW_WIDTH - COURT_LINE - PAD_THICK
+right_pad_y = (VIEW_HEIGHT - R_PAD_LEN) / 2
+# Start getting drawing unit for the right paddle
+r_pad = pyg.Rect(right_pad_x, right_pad_y, PAD_THICK, R_PAD_LEN)
 
-right_pad_x = WINDOWWIDTH - BOUNDTHICKNESS - LINETHICKNESS
-right_pad_y = (WINDOWHEIGHT - RPADDLESIZE) / 2
-LPaddlePosition_X = BOUNDTHICKNESS
+# Left paddle sizing
+left_pad_x = COURT_LINE
+left_pad_y = (VIEW_HEIGHT - L_PAD_LEN) / 2
+l_pad = pyg.Rect(left_pad_x, left_pad_y, PAD_THICK, L_PAD_LEN)
 
-LPaddlePosition_Y = (WINDOWHEIGHT - LPADDLESIZE) / 2
+# Start Training
+print("Start the training session, current set training trails: ", TRAIN_TRAIL, " times")
+pc.simulated_training(TRAIN_TRAIL, Q_Dict, Action_Dict)
+
+# Set up the initial state after the training to start testing
+print("The training session has been completed.")
+round_count = 1
+init_state = (0.5, 0.5, 0.03, 0.01, 0.5 - (pc.RP_HEIGHT / 2), 0.5 - (pc.LP_HEIGHT / 2))
+discrete_init = pc.to_discrete(init_state)
+
+displayState(init_state, l_pad, r_pad, round_count)
+
+right_action = max(Q_Dict[discrete_init], key=Q_Dict[discrete_init].get)
+left_action = pc.l_paddle_action(init_state)
+action = (right_action, left_action)
+state = pc.action_state(init_state, action)
+prev_state = init_state
+
+flag = True
+
+# Start looping this game
+while flag:
+    # Make sure that the user doesn't want to quit
+    for event in pyg.event.get():
+        if event.type == pyg.QUIT:
+            pyg.quit()
+            sys.exit(status=None)
+
+    # Update the display
+    displayState(state, l_pad, r_pad, round_count)
+
+    bounce_count = 0
+
+    state, prev_state, right_action = pc.update_pos(prev_state, right_action, state, Q_Dict, Action_Dict)
+
+    # Check if the AI lose the game
+    if right_action == "END":
+        prev_state = init_state
+        displayState(prev_state, l_pad, r_pad, round_count)
+        round_count += 1
+        right_action = max(Q_Dict[discrete_init], key=Q_Dict[discrete_init].get)
+        left_action = pc.l_paddle_action(prev_state)
+        action = (right_action, left_action)
+        curr_state = pc.action_state(prev_state, action)
+        bounce_count = 0
+    if pc.is_bounced(prev_state, curr_state) and curr_state[3] < 0:
+        bounce_count += 1
+        print("Paddle bounces the ball %s", format(bounce_count), " times")
+
+    pyg.display.update()
+    clock.tick(FPS)
